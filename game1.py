ae5e2d0ef79adabe129f935afcefb970a5d2a089
MolokovAlex <milky77@mail.ru>
2022-04-14 16:42:05 +0300

    refactoring

--- game1.py
+++ game1.py
@@ -69,7 +69,9 @@ def showScore(scoreUser: int)-> None:
     screen.blit(text2, [70,20])
 
 
-FPS = 1
+FPS = 1                     # максимальный FPS, быстрее которого программа работать не будет
+X_window = 500              # размеры окна игры
+Y_window = 500
 RED = (255, 0, 0)
 BLUE = (0, 0, 255)
 YELLOW = (255, 255, 0)
@@ -84,31 +86,7 @@ score = 0
 # После импорта библиотеки, необходимо её инициализировать:
 pygame.init()
 # И создать окно:
-#screen = pygame.display.set_mode((1200, 900))
-screen = pygame.display.set_mode((500, 500))
-
-# здесь будут рисоваться фигуры для изображения дома
-# ...
-# rect(screen, (255, 0, 255), (100, 100, 200, 200))
-# rect(screen, (0, 0, 255), (100, 100, 200, 200), 5)
-# polygon(screen, (255, 255, 0), [(100,100), (200,50),
-#                                (300,100), (100,100)])
-# polygon(screen, (0, 0, 255), [(100,100), (200,50),
-#                                (300,100), (100,100)], 5)
-# circle(screen, (0, 255, 0), (200, 175), 50)
-# circle(screen, (255, 255, 255), (200, 175), 50, 5)
-
-# Для создания штриховок можно использовать циклы
-# x1 = 100; y1 = 100
-# x2 = 300; y2 = 200
-# N = 10
-# color = (255, 255, 255)
-# rect(screen, color, (x1, y1, x2 - x1, y2 - y1), 2)
-# h = (x2 - x1) // (N + 1)
-# x = x1 + h
-# for i in range(N):
-#     line(screen, color, (x, y1), (x, y2))
-#     x += h
+screen = pygame.display.set_mode((X_window, Y_window))
 
 # после чего, чтобы они отобразились на экране, экран нужно обновить:
 pygame.display.update()
@@ -118,28 +96,17 @@ clock = pygame.time.Clock()         # добавление небольшой з
                                     # "вхолостую", постоянно считывая события, которых, скорее всего, нет. 
                                     # Для этого в pygame есть специальный модуль time. До начала главного цикла создаем объект Clock
 
-finished = False
-
-# Наконец, нужно создать основной цикл, в котором будут отслеживаться
+# Нужно создать основной цикл, в котором будут отслеживаться
 # происходящие события.
-# Пока единственное событие, которое нас интересует - выход из программы.
+finished = False
 while not finished:
     clock.tick(FPS)                          # Здесь 30 - это максимальный FPS, быстрее которого программа работать не будет.
-    
-
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             finished = True
         elif event.type == pygame.MOUSEBUTTONDOWN:                          #  если нажата кнопка мыши
-            if (event.button == 1) and (hitClickMouse(event.pos, ball_position_and_radius)):                   #  она левая и произошло попадание в шарик
+            if (event.button == 1) and (hitClickMouse(event.pos, ball_position_and_radius)):     #  она левая и произошло попадание в шарик
                 score +=1
-                print(score)
-#                 circle(screen, RED, event.pos, 50)
-#                 pygame.display.update()
-#             elif event.button == 3:
-#                 circle(screen,  BLUE, event.pos, 50)
-#                 pygame.display.update()
-    
     ball_position_and_radius = new_ball()
     showScore(score)
     pygame.display.update()
